bfed860d187c53757958dbb0b7ccda3c07c026d7
shubhamCda <shubhambodalkar281@gmail.com>
2025-09-01 20:12:17 +0530

    Add union/intersection concept

--- unionINtersection.ts
+++ unionINtersection.ts
@@ -13,4 +13,41 @@ const displayInfo = (value: string | number) => {
 // console.log(displayInfo("Shubhambodalkar"));
 // console.log(displayInfo(true));
 
+//////////////////////////////////////////////////
+
+// Union
+
+type Person = {
+  name: string;
+  age: number;
+}
+
+type Employee = {
+  emp_id: number;
+  department: string;
+}
+
+// type EmployeeDetails = Person | Employee;
+
+// const employee: EmployeeDetails = {
+//   name: "Shubh",
+//   age: 31,
+//   emp_id: 1012
+// }
+
+// Intersection
+
+type EmployeeDetails = Person & Employee;
+
+// we have to mention all the properties from type Person and Employee otherwise employee obj. will show error.
+
+const employee: EmployeeDetails = {
+  name: "Shubh",
+  age: 31,
+  emp_id: 1012,
+  department: "IT"
+}
+
+//this contains all the properties
+
 
