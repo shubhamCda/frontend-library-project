760535573ef3ff132332b3a37a8e1bfa37b2d7d3
shubhamCda <shubhambodalkar281@gmail.com>
2025-09-03 13:47:50 +0530

    Create function to fetch data in TS

--- project/dist/index.js
+++ project/dist/index.js
@@ -1,4 +1,20 @@
 "use strict";
-
-console.log("Hello, I'm Shubham");
+Object.defineProperty(exports, "__esModule", { value: true });
+const searchElement = document.querySelector("#user");
+const formSubmit = document.querySelector("#form");
+const mainContainer = document.querySelector(".main-container");
+const dataURL = "https://api.github.com/users";
+async function customFetchData(url, options) {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    if (!response.ok) {
+        throw new Error("Network issue...please check!!!");
+    }
+    console.log(result);
+    return result;
+}
+function fetchUserData(url) {
+    customFetchData(url, {});
+}
+fetchUserData(dataURL);
 //# sourceMappingURL=index.js.map
--- project/src/index.ts
+++ project/src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello, I'm Shubham");
+const searchElement = document.querySelector("#user") as HTMLInputElement;
+const formSubmit = document.querySelector("#form") as HTMLFormElement;
+const mainContainer = document.querySelector(".main-container") as HTMLElement;
+
+const dataURL = "https://api.github.com/users";
+
+interface userData {
+  id: string;
+  login: string;
+  avatar_url: string;
+  url: string;
+}
+
+
+
+async function customFetchData<T>(url:string, options?:RequestInit):Promise<T> {
+  const response = await fetch(url, options);
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Network issue...please check!!!");
+    
+  }
+  console.log(result);
+  return result
+  
+}
+
+
+function fetchUserData(url:string) {
+  customFetchData<userData[]>(url, {});
+}
+
+
+fetchUserData(dataURL);
