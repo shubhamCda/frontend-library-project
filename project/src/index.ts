a7ca096ed668a37939a35332a740d16ff5f45f4e
shubhamCda <shubhambodalkar281@gmail.com>
2025-09-03 14:29:00 +0530

    Add function to generate UI

--- project/src/index.ts
+++ project/src/index.ts
@@ -4,32 +4,53 @@ const mainContainer = document.querySelector(".main-container") as HTMLElement;
 
 const dataURL = "https://api.github.com/users";
 
-interface userData {
-  id: string;
-  login: string;
-  avatar_url: string;
-  url: string;
+interface UserData {
+	id: string;
+	login: string;
+	avatar_url: string;
+	url: string;
 }
 
+async function customFetchData<T>(
+	url: string,
+	options?: RequestInit
+): Promise<T> {
+	const response = await fetch(url, options);
+	const result = await response.json();
 
-
-async function customFetchData<T>(url:string, options?:RequestInit):Promise<T> {
-  const response = await fetch(url, options);
-  const result = await response.json();
-
-  if (!response.ok) {
-    throw new Error("Network issue...please check!!!");
-    
-  }
-  console.log(result);
-  return result
-  
+	if (!response.ok) {
+		throw new Error("Network issue...please check!!!");
+	}
+	console.log(result);
+	return result;
 }
 
+function generateUI(singleUser: UserData) {
+  const { id, avatar_url, login, url } = singleUser;
 
-function fetchUserData(url:string) {
-  customFetchData<userData[]>(url, {});
+	mainContainer.insertAdjacentHTML(
+		"beforeend",
+		`
+    <div class="card">
+			<img src="${avatar_url}" alt="${login}">
+			<hr/>
+			<div class="card-footer">
+				<img src="${avatar_url}" alt="${login}">
+				<a href="${url}">Github</a>
+			</div>
+		</div>
+    `
+	);
 }
 
+function fetchUserData(url: string) {
+	customFetchData<UserData[]>(url, {}).then((user) => {
+		for (const singleUser of user) {
+      generateUI(singleUser);
+      console.log(`user:${singleUser}`);
+      
+		}
+	});
+}
 
 fetchUserData(dataURL);
