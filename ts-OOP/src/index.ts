297101c8f0c20e08d352d045c9536bd0dd618616
shubhamCda <shubhambodalkar281@gmail.com>
2025-09-05 12:09:02 +0530

    Add class inheritance

--- ts-OOP/src/index.ts
+++ ts-OOP/src/index.ts
@@ -1,3 +1,25 @@
+//class and object creation
+
+// class Person {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+
+//   constructor(name: string, age: number, hobbies: string[]) {
+//     this.name = name;
+//     this.age = age;
+//     this.hobbies = hobbies;
+//   }
+// }
+
+// const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
+
+// console.log(person1);
+
+//////////////////////////////////////////////////////////////////////////////
+
+//Inheritance
+
 class Person {
   name: string;
   age: number;
@@ -8,8 +30,30 @@ class Person {
     this.age = age;
     this.hobbies = hobbies;
   }
+
+  introduction():string {
+    return `My name is ${this.name}, I'm ${this.age} old. My hobbies are ${this.hobbies.join(", ")}.`
+  }
 }
+
+class Student extends Person{
+  grade: number;
+
+  constructor(name: string, age: number, hobbies: string[], grade: number) {
+    super(name, age, hobbies);
+    this.grade = grade;
+  }
+
+  introduction(): string {
+    return `${super.introduction()}. My grade is ${this.grade}.`;
+  }
+}
+
 
 const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
 
-console.log(person1);
+console.log(person1.introduction());
+
+const studen1 = new Student("Shubham", 28, ["coding", "movie", "trekking"], 10);
+console.log(studen1.introduction());
+
