888a49073f8777b12ef412773324b013f96f390e
shubhamCda <shubhambodalkar281@gmail.com>
2025-09-05 11:24:18 +0530

    Add dist js files

---
+++ ts-OOP/dist/index.js
@@ -0,0 +1,14 @@
+class Person {
+    name;
+    age;
+    hobbies;
+    constructor(name, age, hobbies) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+}
+const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
+console.log(person1);
+export {};
+//# sourceMappingURL=index.js.map
