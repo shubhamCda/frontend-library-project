c05a5893f11671a85a689591a66789fd270130e1
shubhamCda <shubhambodalkar281@gmail.com>
2025-09-05 13:26:39 +0530

    Add shorthand method

--- ts-OOP/dist/index.js
+++ ts-OOP/dist/index.js
@@ -13,14 +13,80 @@
 // console.log(person1);
 //////////////////////////////////////////////////////////////////////////////
 //Inheritance
+// class Person {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   constructor(name: string, age: number, hobbies: string[]) {
+//     this.name = name;
+//     this.age = age;
+//     this.hobbies = hobbies;
+//   }
+//   introduction():string {
+//     return `My name is ${this.name}, I'm ${this.age} old. My hobbies are ${this.hobbies.join(", ")}.`
+//   }
+// }
+// class Student extends Person{
+//   grade: number;
+//   constructor(name: string, age: number, hobbies: string[], grade: number) {
+//     super(name, age, hobbies);
+//     this.grade = grade;
+//   }
+//   introduction(): string {
+//     return `${super.introduction()}. My grade is ${this.grade}.`;
+//   }
+// }
+// const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
+// console.log(person1.introduction());
+// const studen1 = new Student("Shubham", 28, ["coding", "movie", "trekking"], 10);
+// console.log(studen1.introduction());
+//////////////////////////////////////////////////////////////////////////////////////////
+//Access modifiers
+// class Person {
+//   public name: string;
+//   public age: number;
+//   protected hobbies: string[];
+//   constructor(name: string, age: number, hobbies: string[]) {
+//     this.name = name;
+//     this.age = age;
+//     this.hobbies = hobbies;
+//   }
+//   introduction():string {
+//     return `My name is ${this.name}, I'm ${this.age} old. My hobbies are ${this.hobbies.join(", ")}.`
+//   }
+// }
+// class Student extends Person{
+//   public grade: number;
+//   constructor(name: string, age: number, hobbies: string[], grade: number) {
+//     super(name, age, hobbies);
+//     this.grade = grade;
+//   }
+//   introduction(): string {
+//     return `${super.introduction()}. My grade is ${this.grade}.`;
+//   }
+// }
+// const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
+// console.log(person1.introduction());
+// const studen1 = new Student("Shubham", 28, ["coding", "movie", "trekking"], 10);
+// console.log(studen1.introduction());
+////////////////////////////////////////////////////////////////////////////////
+//Short-hand method
 class Person {
     name;
     age;
     hobbies;
+    // public name: string;
+    // public age: number;
+    // protected hobbies: string[];
+    //we can directly declare properties in constructor parameters with access modifiers
     constructor(name, age, hobbies) {
         this.name = name;
         this.age = age;
         this.hobbies = hobbies;
+        // this.name = name;
+        // this.age = age;
+        // this.hobbies = hobbies;
+        // there is no need to declare
     }
     introduction() {
         return `My name is ${this.name}, I'm ${this.age} old. My hobbies are ${this.hobbies.join(", ")}.`;
@@ -28,6 +94,7 @@ class Person {
 }
 class Student extends Person {
     grade;
+    // public grade: number;
     constructor(name, age, hobbies, grade) {
         super(name, age, hobbies);
         this.grade = grade;
--- ts-OOP/src/index.ts
+++ ts-OOP/src/index.ts
@@ -20,35 +20,122 @@
 
 //Inheritance
 
+// class Person {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+
+//   constructor(name: string, age: number, hobbies: string[]) {
+//     this.name = name;
+//     this.age = age;
+//     this.hobbies = hobbies;
+//   }
+
+//   introduction():string {
+//     return `My name is ${this.name}, I'm ${this.age} old. My hobbies are ${this.hobbies.join(", ")}.`
+//   }
+// }
+
+// class Student extends Person{
+//   grade: number;
+
+//   constructor(name: string, age: number, hobbies: string[], grade: number) {
+//     super(name, age, hobbies);
+//     this.grade = grade;
+//   }
+
+//   introduction(): string {
+//     return `${super.introduction()}. My grade is ${this.grade}.`;
+//   }
+// }
+
+// const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
+
+// console.log(person1.introduction());
+
+// const studen1 = new Student("Shubham", 28, ["coding", "movie", "trekking"], 10);
+// console.log(studen1.introduction());
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//Access modifiers
+
+// class Person {
+//   public name: string;
+//   public age: number;
+//   protected hobbies: string[];
+
+//   constructor(name: string, age: number, hobbies: string[]) {
+//     this.name = name;
+//     this.age = age;
+//     this.hobbies = hobbies;
+//   }
+
+//   introduction():string {
+//     return `My name is ${this.name}, I'm ${this.age} old. My hobbies are ${this.hobbies.join(", ")}.`
+//   }
+// }
+
+// class Student extends Person{
+//   public grade: number;
+
+//   constructor(name: string, age: number, hobbies: string[], grade: number) {
+//     super(name, age, hobbies);
+//     this.grade = grade;
+//   }
+
+//   introduction(): string {
+//     return `${super.introduction()}. My grade is ${this.grade}.`;
+//   }
+// }
+
+// const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
+
+// console.log(person1.introduction());
+
+// const studen1 = new Student("Shubham", 28, ["coding", "movie", "trekking"], 10);
+// console.log(studen1.introduction());
+
+////////////////////////////////////////////////////////////////////////////////
+
+//Short-hand method
+
 class Person {
-  name: string;
-  age: number;
-  hobbies: string[];
+	// public name: string;
+	// public age: number;
+	// protected hobbies: string[];
+	//we can directly declare properties in constructor parameters with access modifiers
 
-  constructor(name: string, age: number, hobbies: string[]) {
-    this.name = name;
-    this.age = age;
-    this.hobbies = hobbies;
-  }
+	constructor(
+		public name: string,
+		public age: number,
+		protected hobbies: string[]
+	) {
+		// this.name = name;
+		// this.age = age;
+    // this.hobbies = hobbies;
+    // there is no need to declare
+	}
 
-  introduction():string {
-    return `My name is ${this.name}, I'm ${this.age} old. My hobbies are ${this.hobbies.join(", ")}.`
-  }
+	introduction(): string {
+		return `My name is ${this.name}, I'm ${
+			this.age
+		} old. My hobbies are ${this.hobbies.join(", ")}.`;
+	}
 }
 
-class Student extends Person{
-  grade: number;
+class Student extends Person {
+	// public grade: number;
 
-  constructor(name: string, age: number, hobbies: string[], grade: number) {
-    super(name, age, hobbies);
-    this.grade = grade;
-  }
+	constructor(name: string, age: number, hobbies: string[], public grade: number) {
+		super(name, age, hobbies);
+		
+	}
 
-  introduction(): string {
-    return `${super.introduction()}. My grade is ${this.grade}.`;
-  }
+	introduction(): string {
+		return `${super.introduction()}. My grade is ${this.grade}.`;
+	}
 }
-
 
 const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
 
@@ -56,4 +143,3 @@ console.log(person1.introduction());
 
 const studen1 = new Student("Shubham", 28, ["coding", "movie", "trekking"], 10);
 console.log(studen1.introduction());
-
