b25a56694342a8d278e7f443a38d52fe1f50fda7
shubhamCda <shubhambodalkar281@gmail.com>
2025-09-05 12:09:35 +0530

    Add neccessary changes

--- ts-OOP/dist/index.js
+++ ts-OOP/dist/index.js
@@ -1,3 +1,18 @@
+//class and object creation
+// class Person {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   constructor(name: string, age: number, hobbies: string[]) {
+//     this.name = name;
+//     this.age = age;
+//     this.hobbies = hobbies;
+//   }
+// }
+// const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
+// console.log(person1);
+//////////////////////////////////////////////////////////////////////////////
+//Inheritance
 class Person {
     name;
     age;
@@ -7,8 +22,23 @@ class Person {
         this.age = age;
         this.hobbies = hobbies;
     }
+    introduction() {
+        return `My name is ${this.name}, I'm ${this.age} old. My hobbies are ${this.hobbies.join(", ")}.`;
+    }
+}
+class Student extends Person {
+    grade;
+    constructor(name, age, hobbies, grade) {
+        super(name, age, hobbies);
+        this.grade = grade;
+    }
+    introduction() {
+        return `${super.introduction()}. My grade is ${this.grade}.`;
+    }
 }
 const person1 = new Person("shubha", 31, ["i", "live", "Goa"]);
-console.log(person1);
+console.log(person1.introduction());
+const studen1 = new Student("Shubham", 28, ["coding", "movie", "trekking"], 10);
+console.log(studen1.introduction());
 export {};
 //# sourceMappingURL=index.js.map
